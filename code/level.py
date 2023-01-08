856b55339f53739af27677ac01c2ad6f345cf60a
Sergey <ser1a@ya.ru>
2023-01-08 13:19:59 +0300

    Реализована камера с центром на игроке и метод отрисовки спрайтов уровня

--- code/level.py
+++ code/level.py
@@ -5,35 +5,53 @@ from tile import Tile
 from player import Player
 from helper_def import import_csv_layout
 
-# создание и отрисовка уровня
 class Level:
     def __init__(self):
-        # получили главный surface с нашего display
         self.screen = pygame.display.get_surface()
 
-        # создаем группу для спрайтов, которые будем рисовать
-        self.visible_sprites = pygame.sprite.Group()
-        # Создаем группу для спрайтов, которые будут препятствиями
+        # создаем специальную группу для наших "видимых" спрайтов
+        # у этой группы мы сделаем свой метод draw, чтобы рисовать со смещением
+        self.visible_sprites = CameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
 
         self.create_map()
 
     def create_map(self):
-        # создаем карту исходя из списка в константе WORLD_MAP
         for row_index, row in enumerate(WORLD_MAP):
             for col_index, col in enumerate(row):
                 x = col_index * TILESIZE
                 y = row_index * TILESIZE
                 if col == 'X':
-                    # наши тайлы X создаются сразу с двумя группами - так,
-                    # как их надо и рисовать, и одновременно они являются препятствием
                     Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
                 if col == 'P':
-                    # игрока сохраним в переменную - в дальнейшем это понадобится
                     self.player = Player((x,y), [self.visible_sprites], self.obstacle_sprites )
 
     def draw(self):
-        # рисуем только группу видимых спрайтов
-        self.visible_sprites.draw(self.screen)
+        # теперь мы рисуем спрайты с помощью нашего отдельного метода
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
         debug(self.player.direction)
+
+# наследуем все свойства и методы от класса Group
+class CameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.screen = pygame.display.get_surface()
+        # узнаем середину экрана
+        self.half_width = self.screen.get_size()[0] // 2
+        self.half_height = self.screen.get_size()[1] // 2
+        # создадим вектор который будет контролировать смещение отрисовки
+        # про вектор можно посмотреть в предыдущих коммитах
+        self.offset = pygame.math.Vector2()
+
+    def custom_draw(self, player):
+        # вычисляем смещение игрока относительно центра
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        # перебираем и отрисовываем каждый спрайт в данной группе
+        # но рисуем с учетом смещения
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft - self.offset
+            self.screen.blit(sprite.image, offset_pos)
+
